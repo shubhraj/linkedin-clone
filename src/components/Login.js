d0931770030f92e5db79840a5896ec859f4ba57d
patilsh <shubham.patil@sincrodigital.com>
2023-04-14 20:27:03 +0530

    adding styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,12 +7,51 @@ const Login = (props) => {
                 <a href='/' >
                     <img src="/images/login-logo.svg"></img>
                 </a>
+                <div>
+                    <Join>Join Now</Join>
+                </div>
             </Nav>
         </Container>
     )
 }
 
-const Container = styled.div``;
-const Nav = styled.nav``;
+const Container = styled.div`
+    padding: 0px;
+`;
+
+const Nav = styled.nav`
+    max-width: 100%;
+    margin: auto;
+    padding: 12px 0 16px;
+    display: flex;
+    align-item: centre;
+    position: relative;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+    
+    & > a {
+        width: 3rem;
+        height: 4rem;
+
+        @media(max-width: 768px) {
+            padding: 0 5px;
+
+        }
+    }
+`;
+
+const Join = styled.a`
+    font-size: 16px;
+    padding: 10px 12px;
+    text-decoration: none;
+    color: rgba(0,0,0,0.6);
+    margin-right: 12px;
+    border-radius: 4px;
+    &:hover {
+        background-color: rgba(0,0,0,0.08);
+        color: rgba(0,0,0,0.9);
+        text-decoration: none;
+    }
+`;
 
 export default Login;
