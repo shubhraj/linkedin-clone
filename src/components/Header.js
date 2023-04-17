fca4c7e652b6561ffff514edd80d48411a61f446
patilsh <shubham.patil@sincrodigital.com>
2023-04-17 15:53:07 +0530

    adding the nav bar items and styling it

--- src/components/Header.js
+++ src/components/Header.js
@@ -47,6 +47,24 @@ const Header = ()=> {
                                 <span>Notifications</span>
                             </a>
                         </NavList>
+                        <User>
+                            <a> 
+                                <img src="/images/user.svg"></img>
+                                <span>Me</span>
+                                <img src="/images/down-icon.svg"></img>
+                            </a>
+
+                            <SignOut><a> Sign Out</a></SignOut>
+                        </User>
+                        <Work> 
+                            <a>
+                                <img src="/images/nav-work.svg"></img>
+                                <span>
+                                    Work
+                                    <img src="/images/down-icon.svg"></img>
+                                </span>
+                            </a>
+                        </Work>
                     </NavListWrap>
                 </Nav>
             </Content>
@@ -135,6 +153,20 @@ const NavListWrap = styled.ul`
     display: flex;
     flex-wrap: nowrap;
     list-style-type: none;
+
+    .active {
+       span:after {
+            content: '';
+            transform: scaleX(1);
+            border-bottom: 2px solid var(--white, #fff);
+            bottom: 0;
+            left: 0;
+            position: absolute;
+            transition: transform 0.2s ease-in-out;
+            width: 100% ; 
+            border-color: rgba(0, 0, 0, 0.9);
+       }
+    }
 `;
 
 const NavList = styled.li`
@@ -174,6 +206,52 @@ const NavList = styled.li`
 
     }
    }
+`;
+
+const SignOut = styled.div`
+    position: absolute;
+    top: 45px;
+    background: white;
+    border-radius: 0 0 5px 5px;
+    width: 100px;
+    height: 40px;
+    font-size: 16px;
+    transition-duration: 167ms;
+    text-align: center;
+    display: none;
+`;
+
+const User = styled(NavList)`
+    a > svg {
+        width:  24px;
+        border-radius: 50%;
+    }
+
+    a > img {
+        width:  24px;
+        height: 24px;
+        border-radius: 50%;
+    }
+
+    span {
+        display: flex;
+        align-items: center;
+    }
+
+    &:hover{
+       ${SignOut} {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+       }
+    }
+    
+`;
+
+
+
+const Work = styled(User)`
+    border-left: 1px solid rgba(0,0,0,0.08 );
 `;
 
 export default Header;
